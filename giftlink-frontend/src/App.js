5db64b26c3437692841f5f265697ac6f0c51ad2f
alekssztkvs <aleksszitkovs@inbox.lv>
2026-04-21 01:40:19 +0300

    auth routes update

--- giftlink-frontend/src/App.js
+++ giftlink-frontend/src/App.js
@@ -8,12 +8,15 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 import './App.css';
 import Navbar from './components/Navbar/Navbar';
 import DetailsPage from './components/DetailsPage/DetailsPage';
+import Profile from './components/Profile/Profile';
+
 function App() {
   const navigate = useNavigate();
   return (
         <>
         <Navbar/>
         <Routes>
+          <Route path="/app/profile" element={<Profile/>} />
           <Route path="/app/search" element={<SearchPage/>} />
           <Route path="/" element={<MainPage />} />
           <Route path="/app" element={<MainPage />} />
